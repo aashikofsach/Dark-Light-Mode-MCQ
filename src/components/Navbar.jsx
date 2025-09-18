e593051ab45a1432567e67368e94f40277d656ab
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-09-18 22:52:13 +0530

    applied the checkbox button to make theme toggle and semicolon issue get corrected in css file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,21 @@ import { Link } from 'react-router-dom'
 import { useTheme } from '../theme-context'
 
 function Navbar() {
- const {theme} =  useTheme();
+ const {theme, toggleTheme} =  useTheme();
  console.log(theme)
-  return (
-    <div className='navbar'>
+  return (<>
+  
+   <div className='navbar'>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/blog">Blog</Link>
     </div>
+    <div>
+      <input type="checkbox" onClick={()=>toggleTheme()} checked={theme==="dark"} />
+    </div>
+  </>
+   
+
   )
 }
 
